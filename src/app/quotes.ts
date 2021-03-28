38264a46cb4a8c11c97919f91c678706dec8e01d
JoyWambui <JoyWambui@github.com>
2021-03-28 19:18:15 +0300

    add quotes constructor

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,11 @@
 export class Quotes {
+  constructor(public id : number, public name : string, public author : string, public submitter : string, public upVote : number, public downVote : number, public published : Date ){
+  this.id =id;
+  this.name = name;
+  this.author =author;
+  this.submitter = submitter;
+  this.upVote =upVote;
+  this.downVote =downVote;
+  this.published= published;
+  }
 }
